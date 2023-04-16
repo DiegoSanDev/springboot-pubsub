05fbe24bb9d47de3f6e17228dfd0ffb3f92c8dda
Diego Santos <diegosan.dev@gmail.com>
2023-04-16 10:19:27 -0300

    ajuste

--- dojo.pubsub.cadastro.usuario/src/main/java/br/com/accava/dojo/pubsub/cadastro/usuario/domain/port/UserRepository.java
+++ dojo.pubsub.cadastro.usuario/src/main/java/br/com/accava/dojo/pubsub/cadastro/usuario/domain/port/UserRepository.java
@@ -3,7 +3,6 @@ package br.com.accava.dojo.pubsub.cadastro.usuario.domain.port;
 import br.com.accava.dojo.pubsub.cadastro.usuario.domain.domain.User;
 
 public interface UserRepository {
-
     void save(User user);
 
 }
